e38cd5a7e2134db135662e0a3e17760dd178b548
DanielWangZhanggui <danielwzhg@gmail.com>
2022-11-16 10:38:56 +0800

    add error hanlding

--- ImageProcessor/index.js
+++ ImageProcessor/index.js
@@ -3,12 +3,10 @@ var Jimp = require("jimp");
 
 
 module.exports = async function (context, req) {
-    // const orgUrl = req.originalUrl;
-    // const blobPath = orgUrl.split('ImageProcessor')[1].split('?')[0]
-    // context.log(blobPath);
-    context.log(context.bindingData.blobContainer);
-    context.log(context.bindingData.blobPath);
-
+    //Print Blob Container & Path
+    context.log("blobContainer:\t" + context.bindingData.blobContainer);
+    context.log("blobPath:\t" + context.bindingData.blobPath);
+    //Print  QueryString from URL
     context.log(req.query);
     var blob = "https://"+ process.env["StorageAccountName"] + ".blob" + process.env["StorageAccountSuffix"] + "/" + context.bindingData.blobContainer + "/" + context.bindingData.blobPath;
     queryParams = req.query;
@@ -17,34 +15,45 @@ module.exports = async function (context, req) {
         blob = blob + "?sp=" + req.query.sp + "&st=" + req.query.st + "&se=" + req.query.se + "&spr=" + req.query.spr + "&sv=" + req.query.sv + "&sr="+ req.query.sr + "&sig=" + encodeURIComponent(req.query.sig);  
     }
     
+    //Print the total Blob URL 
     context.log(blob);
-    const image = await Jimp.read(blob);
-    // const image = await Jimp.read('https://ausstorageaccounta.blob.core.windows.net/privpic/pexels-photo-sample.jpeg?sp=r&st=2022-11-15T03:49:20Z&se=2022-11-15T11:49:20Z&spr=https&sv=2021-06-08&sr=c&sig=zrvpenz9UYJniPK817Jn185VrgzdvrSX7Vpazw3RHfg%3D');
-    // await image.resize(150, 150);
+    const imageType = context.bindingData.blobPath.split(".")[1];
+    try{
+        //read image from azure blob storage
+        const image = await Jimp.read(blob);
+        if(typeof req.query.p !== 'undefined' && req.query.p)
+        {
+            await image.scale(Number(req.query.p), Jimp.RESIZE_BEZIER);
+        }
+        else if(typeof req.query.w !== 'undefined' && req.query.w)
+        {
+            await image.resize(Number(req.query.w), Number(req.query.h));
+        }
+        
+        var buffer; 
+        image.getBuffer(Jimp.MIME_PNG, (err, result)=>{
+        buffer = result;
+        });
 
-    if(typeof req.query.p !== 'undefined' && req.query.p)
-    {
-        await image.scale(Number(req.query.p), Jimp.RESIZE_BEZIER);
+        context.res = {
+            headers: {
+                "Content-Type": "image/"+imageType
+            },
+            isRaw: true,
+            // status: 200, /* Defaults to 200 */
+            body: new Uint8Array(buffer)
+        };
     }
-    else if(typeof req.query.w !== 'undefined' && req.query.w)
-    {
-        await image.resize(Number(req.query.w), Number(req.query.h));
+    catch{
+        console.log("Error while getting the image");
+        context.res = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 401,
+            body: "Error while getting the image"
+        };
     }
+
     
-    // await image.scale(0.5, Jimp.RESIZE_BEZIER);
-    // await image.writeAsync(`test/${Date.now()}_0.5.png`);
-
-    var buffer; 
-    image.getBuffer(Jimp.MIME_PNG, (err, result)=>{
-       buffer = result;
-    });
-
-    context.res = {
-        headers: {
-            "Content-Type": "image/png"
-        },
-        isRaw: true,
-        // status: 200, /* Defaults to 200 */
-        body: new Uint8Array(buffer)
-    };
 }
